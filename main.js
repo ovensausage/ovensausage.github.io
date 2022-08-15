9ed056f84a8149831f2dcc2a0b071e784d19e916
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2022-08-15 18:57:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function press1() {
-document.getElementById("press").innerHTML = "Javascript Rules"; 
+document.getElementById("press").innerHTML = "Javascript Rules. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus nulla metus. Nunc eleifend"; 
 }
 function press2() {
 document.getElementById("press").innerHTML = "I am a web developer and software engineer focusing in HTML(as well as CSS and Javascript) and Python respectively. My current projects are this website and  some python scripts.";
