07137d0245f0455ab7135001d662fb2f31263444
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2022-08-16 17:04:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ document.getElementById("press").innerHTML = "I am a web developer and software 
 function drop() {
   document.getElementById("drop").classList.toggle("show");
 }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
