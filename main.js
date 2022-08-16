12ab0873ca38657e95f209484c78d22085f40434
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2022-08-16 18:40:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ document.getElementById("press").innerHTML = "Javascript Rules!!!"
 function press2() {
 document.getElementById("press").innerHTML = "I am a web developer and software engineer focusing in HTML (as well as CSS and Javascript) and Python respectively. My current projects are this website and  some python scripts.";
 }
-function drop() {
-  document.getElementById("drop").classList.toggle("show");
-  document.getElementById("drop").id = "drop2";
-  document.getElementById("drop2").classList.remove("show");
-  document.getElementById("drop2").id = "drop";
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
